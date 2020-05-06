76a37d09fec26bb787d66375c81aef4ff5b19cd0
Garrett Luu <garrettluu131@gmail.com>
2020-05-05 18:15:08 -0700

    Changed date field to use date picker

--- lib/main.dart
+++ lib/main.dart
@@ -324,10 +324,10 @@ class EditTask extends StatefulWidget {
 
 class _EditTaskState extends State<EditTask> {
   final controllerName = TextEditingController();
-
   final controllerTime = TextEditingController();
-
   final controllerDue = TextEditingController();
+
+  DateTime _due;
 
   @override
   void dispose() {
@@ -362,7 +362,7 @@ class _EditTaskState extends State<EditTask> {
                     icon: Icons.subject,
                     title: "Task Name",
                     input: TextField(
-                      controller: controllerName,
+                      controller: controllerDue,
                       decoration: InputDecoration(
                         border: InputBorder.none,
                         hintText: "Enter a task name",
@@ -372,13 +372,13 @@ class _EditTaskState extends State<EditTask> {
                   IconInput(
                     icon: Icons.event,
                     title: "Due Date",
-                    input: TextField(
-                      controller: controllerDue,
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: "Enter a task name",
-                      ),
-                    )
+                    input: DatePicker(
+                      updateState: (date) {
+                        setState(() {
+                          _due = date;
+                        });
+                      },
+                    ),
                   ),
                   IconInput(
                     icon: Icons.access_time,
@@ -451,6 +451,55 @@ class  IconInputState extends State<IconInput> {
              ),
              widget.input,
            ],
+        ),
+      ),
+    );
+  }
+}
+
+class DatePicker extends StatefulWidget {
+  DatePicker({Key key, this.updateState}) : super(key: key);
+  final updateState;
+
+  @override
+  _DatePickerState createState() => _DatePickerState();
+}
+
+class _DatePickerState extends State<DatePicker> {
+  DateTime _dateTime = DateTime.now();
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        showDatePicker(
+          context: context,
+          initialDate: DateTime.now(),
+          firstDate: DateTime(2010),
+          lastDate: DateTime(2030),
+        ).then((date) {
+          if (date != null) {
+            if (widget.updateState != null) {
+              widget.updateState(date);
+            }
+            setState(() {
+              _dateTime = date;
+            });
+          }
+        });
+      },
+      child: Align(
+        alignment: Alignment.topLeft,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 16.0),
+          child: Text(
+            _dateTime.toString(),
+            textAlign: TextAlign.left,
+            style: TextStyle(
+              color: Theme.of(context).hintColor,
+              fontSize: 16,
+            ),
+          ),
         ),
       ),
     );
